657a7c74033ace83b9809956eb1ecfd10ec50b59
Dimitri <habimm@pm.me>
2023-02-25 13:19:38 +0700

    remove some PDF to JPG conversion code

--- conv.py
+++
@@ -1,8 +0,0 @@
-from wand.image import Image
-
-with Image(filename='resources/Steuernummer vom Finanzamt.pdf', resolution=300) as pdf:
-  for i, page in enumerate(pdf.sequence):
-    with Image(page) as img:
-      img.compression_quality = 100
-      img.format = 'jpg'
-      img.save(filename=f'output_{i+1}.jpg')
